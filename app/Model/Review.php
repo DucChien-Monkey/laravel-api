824f47166bbe0a190465c2f07e00fae20b5b4ec0
DucChien-Monkey <chien.nguyen@hoc10.com>
2022-05-19 00:50:16 +0700

    relationShip table

--- app/Model/Review.php
+++ app/Model/Review.php
@@ -2,9 +2,13 @@
 
 namespace App\Model;
 
+use App\Model\Blog;
 use Illuminate\Database\Eloquent\Model;
 
 class Review extends Model
 {
     //
+    public function blog() {
+        return $this->belongsTo(Blog::class);
+    }
 }
