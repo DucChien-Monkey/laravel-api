f87d70b87b75af0db5d8dd1e7ab3b7cc127060b1
DucChien-Monkey <chien.nguyen@hoc10.com>
2022-05-18 23:51:10 +0700

    create seeder

--- database/factories/ReviewFactory.php
+++ database/factories/ReviewFactory.php
@@ -4,9 +4,17 @@
 
 use App\Model\Review;
 use Faker\Generator as Faker;
+use App\Model\Blog;
 
 $factory->define(Review::class, function (Faker $faker) {
     return [
         //
+        'blog_id'=> function(){
+        return Blog::all()->random();
+        },
+        'customer'=>$faker->name,
+        'review'=>$faker->paragraph,
+        'star' =>$faker->numberBetween(0,5),
+
     ];
 });
